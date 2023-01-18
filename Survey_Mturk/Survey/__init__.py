d71468965e818abb0275197a5df63770dade5505
Argun Aman <argunamanli@gmail.com>
2023-01-18 14:56:32 +0100

    finished task pics

--- Survey_Mturk/Survey/__init__.py
+++ Survey_Mturk/Survey/__init__.py
@@ -28,27 +28,21 @@ class Player(BasePlayer):
 
     'The following are hidden fields which we will manually assign after the participant confirms the slider input.'
     #choices
-    choice_NV_task=models.StringField()
-    choice_Maze_task=models.StringField()
-    choice_Count_letters_task=models.StringField()
-    choice_Word_puzzle_task=models.StringField()
-    choice_Word_order_task=models.StringField()
-    choice_Count_numbers_task=models.StringField()
-    choice_Single_digit_calculus_task=models.StringField()
-    choice_Ball_bucket_task=models.StringField()
-    choice_Word_in_word_task=models.StringField()
-    choice_Numbers_in_numbers_task=models.StringField()
-    choice_MRT_task=models.StringField()
+    NV_task=models.StringField()
+    Maze_task=models.StringField()
+    Count_letters_task=models.StringField()
+    Word_puzzle_task=models.StringField()
+    Word_order_task=models.StringField()
+    Count_numbers_task=models.StringField()
+    Ball_bucket_task=models.StringField()
+    Word_in_word_task=models.StringField()
+    Numbers_in_numbers_task=models.StringField()
+    MRT_task=models.StringField()
 
 #### Functions and variables
-#list of 10 unique tasks
-tasks=['choice_NV_task','choice_Maze_task','choice_Count_letters_task','choice_Word_order_task','choice_Count_numbers_task',
-       'choice_Single_digit_calculus_task','choice_Ball_bucket_task','choice_Word_in_word_task','choice_Numbers_in_numbers_task','choice_MRT_task']
-
-
-# def get_task():
-#     'this function cycles through tasks and returns the next task from tasks[]'
-#     return next(task)
+#list of 9 unique tasks
+tasks=['NV_task', 'Maze_task','Count_letters_task','Word_puzzle_task','Word_order_task',
+        'Count_numbers_task','Ball_bucket_task','Word_in_word_task','Numbers_in_numbers_task','MRT_task']
 
 
 class Introduction(Page):
@@ -65,27 +59,24 @@ class Choice(Page):
     form_fields = tasks
 
     @staticmethod
-    def vars_for_template(player: Player, form_fields=form_fields,Tasks_path=C.Tasks_path):
+    def vars_for_template(player: Player, form_fields=form_fields, tasks_path=C.Tasks_path):
         round_number = player.round_number
-        task = tasks[round_number]
+        task = tasks[round_number-1]
 
-        path_task = Tasks_path + task + '.html'
+        path_task = tasks_path + task + '.html'
         return dict(
             path_task=path_task,
-            task_name=task
+            task_name=task,
+            round_number=round_number
         )
 
+    @staticmethod
+    def js_vars(player: Player):
+        dict = {'tasks': tasks, 'round_number': player.round_number}
+        return dict
 
 
-# class Choice2(Page):
-#     form_model = 'player'
-#     form_fields =  ['choice_NV_task', 'choice_Maze_task']
-#
-#     @staticmethod
-#     def vars_for_template(player: Player, form_fields=form_fields,Tasks_path=C.Tasks_path):
-#         path_task = Tasks_path + str(form_fields[1]) + '.html'
-#         return dict(path_task=path_task)
-#
+
 
 class Results(Page):
     @staticmethod
