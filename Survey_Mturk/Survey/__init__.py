d47d713a1ef0aeb20b002ee9f5af43f85a462f54
Argun <argunamanli@gmail.com>
2023-01-20 17:40:09 +0100

    created multiple page one round

--- Survey_Mturk/Survey/__init__.py
+++ Survey_Mturk/Survey/__init__.py
@@ -8,13 +8,32 @@ Survey for Mturk for the stereotypes project. Michael Hilweg, Argun Aman 2023
 #todo: think about comprehension and attention checks
 #todo write code for how long each question took to answer per participant.
 #todo: it seems using multiple rounds creates multiple players
+#note: participation fee is in the session configs #todo make sure this is completion fee not mere participation fee
+
 class C(BaseConstants):
     NAME_IN_URL = 'Survey'
     PLAYERS_PER_GROUP = None
-    NUM_ROUNDS = 9
+    NUM_ROUNDS = 3
     Tasks_path= 'Survey/tasks/'
     Instruction_path='_templates/instructions.html'
-    #note: participation fee is in the session configs #todo make sure this is completion fee not mere participation fee
+    #list of questions that will be used in the survey
+    Tasks= ['NV_task', 'Maze_task', 'Count_letters_task', 'Word_puzzle_task', 'Word_order_task',
+             'Count_numbers_task', 'Ball_bucket_task', 'Word_in_word_task', 'Numbers_in_numbers_task', 'MRT_task']
+
+    # Dictionary of true score differences between men and women to be used to calculate payoffs.
+    # Positive x implies men answered x percentage points more.
+    true_difference_list = {
+        'NV_task': 0.05,
+        'Maze_task': 0.11,
+        'Count_letters_task': -0.07,
+        'Word_puzzle_task': 0,
+        'Word_order_task': 0.12,
+        'Count_numbers_task': 0,
+        'Ball_bucket_task': 0,
+        'Word_in_word_task': 0,
+        'Numbers_in_numbers_task': 0,
+        'MRT_task': 0.15
+    }
 
 class Subsession(BaseSubsession):
     pass
@@ -45,93 +64,160 @@ class Player(BasePlayer):
 
 #### Functions and variables
 
-# list of 9 unique tasks, defined on the participant level to to allow shuffling is in the settings menu
-tasks = ['NV_task', 'Maze_task', 'Count_letters_task', 'Word_puzzle_task', 'Word_order_task',
-             'Count_numbers_task', 'Ball_bucket_task', 'Word_in_word_task', 'Numbers_in_numbers_task', 'MRT_task']
 #todo: make randomize the order make it balanced
 #todo: make sure the participant can only move to next page after moving the slider.
 
-#Dictionary of true score differences between men and women to be used to calculate payoffs.
-# Positive x implies men answered x percentage points more.
-true_difference_list={
-    'NV_task': 0.05,
-    'Maze_task': 0.11,
-    'Count_letters_task': -0.07,
-    'Word_puzzle_task':0,
-    'Word_order_task':0.12,
-    'Count_numbers_task':0,
-    'Ball_bucket_task':0,
-    'Word_in_word_task':0,
-    'Numbers_in_numbers_task':0,
-    'MRT_task':0.15
-}
+
+
 
 
 class Introduction(Page):
     form_model = 'player'
     form_fields = ['gender', 'age']
 
-    @staticmethod
-    def is_displayed(player: Player):
-        return player.round_number == 1
 
 
-class Choice(Page):
+
+class Ball_bucket_page(Page):
+    task= 'Ball_bucket_task'
     form_model = 'player'
-    @staticmethod
-    def get_form_fields(player: Player):
-        'dynamically setting the formfield to depend on the round number.'
-        current_task = tasks[player.round_number-1]
-        # if player.round_number>1: #if we are in round 2 then current player has access to player 1 (who's the same participant) fields
-        #     prev_player = player.in_round(player.round_number - 1)
-        # else:
-        #     prev_player = player
-        #
-        # if player.round_number>1:
-        #     for task in tasks[0:player.round_number-2]:
-        #         player.task= getattr(prev_player, task)
-        return [current_task]
-
+    form_fields = [task]
 
     @staticmethod
-    def vars_for_template(player: Player, tasks_path=C.Tasks_path):
-        '''
-        1. need the path_task to display the html
-        2. need round_number to select the current task using JS
-        '''
-        round_number = player.round_number
-        task = tasks[round_number-1]
+    def vars_for_template(player: Player, tasks_path=C.Tasks_path, task=task):
         path_task = tasks_path + task + '.html'
-        return dict(
-            path_task=path_task,
-            round_number=round_number,
-        )
+        return dict(path_task=path_task)
+
+    
+
+class Count_letters_page(Page):
+    task= 'Count_letters_task'
+    form_model = 'player'
+    form_fields = [task]
 
     @staticmethod
-    def js_vars(player: Player):
-        'i use the round_number and list of tasks to select the current task using JS, for this i need to pass these to JS in the page'
-        dict = {'tasks': tasks, 'round_number': player.round_number}
-        return dict
+    def vars_for_template(player: Player, tasks_path=C.Tasks_path, task=task):
+        path_task = tasks_path + task + '.html'
+        return dict(path_task=path_task)
 
+    
+
+class Count_numbers_page(Page):
+    task= 'Count_numbers_task'
+    form_model = 'player'
+    form_fields = [task]
+
+    @staticmethod
+    def vars_for_template(player: Player, tasks_path=C.Tasks_path, task=task):
+        path_task = tasks_path + task + '.html'
+        return dict(path_task=path_task)
+
+    
+
+class Maze_page(Page):
+    task= 'Maze_task'
+    form_model = 'player'
+    form_fields = [task]
+
+    @staticmethod
+    def vars_for_template(player: Player, tasks_path=C.Tasks_path, task=task):
+        path_task = tasks_path + task + '.html'
+        return dict(path_task=path_task)
+
+    
+
+class MRT_page(Page):
+    task= 'MRT_task'
+    form_model = 'player'
+    form_fields = [task]
+
+    @staticmethod
+    def vars_for_template(player: Player, tasks_path=C.Tasks_path, task=task):
+        path_task = tasks_path + task + '.html'
+        return dict(path_task=path_task)
+
+    
+
+class Numbers_in_numbers_page(Page):
+    task= 'Numbers_in_numbers_task'
+    form_model = 'player'
+    form_fields = [task]
+
+    @staticmethod
+    def vars_for_template(player: Player, tasks_path=C.Tasks_path, task=task):
+        path_task = tasks_path + task + '.html'
+        return dict(path_task=path_task)
+
+    
+
+class NV_page(Page):
+    task= 'NV_task'
+    form_model = 'player'
+    form_fields = ['NV_task']
+
+    @staticmethod
+    def vars_for_template(player: Player, tasks_path=C.Tasks_path, task=task):
+        path_task = tasks_path + task + '.html'
+        return dict(path_task=path_task)
+
+
+class Word_in_word_page(Page):
+    task= 'Word_in_word_task'
+    form_model = 'player'
+    form_fields = [task]
+
+    @staticmethod
+    def vars_for_template(player: Player, tasks_path=C.Tasks_path, task=task):
+        path_task = tasks_path + task + '.html'
+        return dict(path_task=path_task)
+
+    
+
+class Word_order_page(Page):
+    task= 'Word_order_task'
+    form_model = 'player'
+    form_fields = [task]
+
+    @staticmethod
+    def vars_for_template(player: Player, tasks_path=C.Tasks_path, task=task):
+        path_task = tasks_path + task + '.html'
+        return dict(path_task=path_task)
+
+    
+
+
+class Word_puzzle_page(Page):
+    task= 'Word_puzzle_task'
+    form_model = 'player'
+    form_fields = [task]
+
+    @staticmethod
+    def vars_for_template(player: Player, tasks_path=C.Tasks_path, task=task):
+        path_task = tasks_path + task + '.html'
+        return dict(path_task=path_task)
+
+    
+
+
+class ResultsWaitPage(Page):
     def before_next_page(player: Player, timeout_happened):
-        'updates the participant\'s payoff'
-        task = tasks[player.round_number - 1] # get the current task name
+        '''
+        updates the participant\'s payoff:
+        1. loop through the tasks in the C.Tasks list
+        2. Get players answer to this task
+        3. Compare players answer to the true difference and assign payoff
+        '''
         # todo: fix the payoff function
-        players_answer = getattr(player, task) #player's answer is stored in player.task field
-        true_difference = true_difference_list[task] #get the true difference from the trie_difference_list
-        earning_from_question = 1 - (true_difference - players_answer)**2 #calculate earnings of participant
-        participant = player.participant #get the participant
-        participant.payoff = participant.payoff + earning_from_question #edit the participants earning
-        print(f" to the task {task} you answered {players_answer} since the true value is {true_difference} you earn 1- ({true_difference} - {players_answer})^2")
-
-
+        for task in C.Tasks:
+            players_answer = getattr(player, task) #player's answer is stored in player.task field
+            true_difference = C.true_difference_list[task] #get the true difference from the trie_difference_list
+            earning_from_question = 1 - (true_difference - players_answer)**2 #calculate earnings of participant
+            participant = player.participant #get the participant
+            participant.payoff = participant.payoff + earning_from_question #edit the participants earning
+            print(f" to the task {task} you answered {players_answer} since the true value is {true_difference} you earn 1- ({true_difference} - {players_answer})^2")
 
 
 class Results(Page):
-    @staticmethod
-    def is_displayed(player:Player):
-        return player.round_number==C.NUM_ROUNDS
-
     @staticmethod
     def vars_for_template(player: Player):
         participant= player.participant
@@ -140,4 +226,5 @@ class Results(Page):
         return ({'participation_fee':participation_fee})
 
 
-page_sequence = [Introduction, Choice, Results]
+page_sequence = [Introduction, NV_page, Maze_page, Count_letters_page, Word_puzzle_page, Word_order_page,
+             Count_numbers_page, Ball_bucket_page, Word_in_word_page, Numbers_in_numbers_page, MRT_page, ResultsWaitPage,  Results]
